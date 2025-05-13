92e62d5be04a968ecdd154b19931e7770aa677be
UdomUdom <udomzaark@gmail.com>
2025-05-14 03:30:17 +0700

    deploy update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,31 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
-
+import node from '@astrojs/node';
+const SERVER_PORT = 4321;
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const LIVE_URL = "https://udomudom.github.io";
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://udomudom.github.io',
-  integrations: [icon(), mdx()]
+
+  server: { port: SERVER_PORT },
+  site: BASE_URL,
+  integrations: [icon(), mdx()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
